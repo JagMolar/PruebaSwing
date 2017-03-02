270227147846be27dcb1f49b7896b26ed7508263
JagMolar <juana.garmue@educa.jcyl.es>
2017-03-02 20:31:48 +0100

    Probando swing1

---
+++ src/pruebaswing/PruebaSwing.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebaswing;
+
+/**
+ *
+ * @author Your Name <Juan A. García Muelas (juangmuelas@hotmail.com)>
+ */
+public class PruebaSwing {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Formulario formulario = new Formulario();
+    }
+    
+}
